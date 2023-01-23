bc133791d5794e249cc6de5c30c39b1c88b1e5ee
William Ralph (40165278) <40165278@hcuc.ac.uk>
2023-01-23 14:21:30 +0000

    First commit

---
+++ Assets/Player.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //kqc3yy is a noob
+        //M-East is a noob
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
