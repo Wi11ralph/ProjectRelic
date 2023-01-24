e007f819a72a5c05352ed260b36c2443cb9a0b74
William Ralph (40165278) <40165278@hcuc.ac.uk>
2023-01-24 09:31:52 +0000

    Added player movement to the player script

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,18 +4,52 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //kqc3yy is a noob
+    //M-East is a noob
+    //SecSad is a noob
+    //TexturephobeCreator is a noob
+    //hm
+
+    [SerializeField] private Rigidbody _rb;
+    [SerializeField] private float _speed = 5;
+    [SerializeField] private float _turnSpeed = 360;
+    private Vector3 _input;
+
+    private void Update()
     {
-        //kqc3yy is a noob
-        //M-East is a noob
-        //SecSad is a noob
-        //TexturephobeCreator is a noob
+        GatherInput();
+        Look();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        Move();
+    }
+
+    private void GatherInput()
+    {
+        _input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+    }
+
+    private void Look()
+    {
+        if (_input == Vector3.zero) return;
+
+        var rot = Quaternion.LookRotation(_input.ToIso(), Vector3.up);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, rot, _turnSpeed * Time.deltaTime);
+    }
+
+    private void Move()
+    {
+        _rb.MovePosition(transform.position + transform.forward * _input.normalized.magnitude * _speed * Time.deltaTime);
     }
 }
+
+public static class Helpers
+{
+    private static Matrix4x4 _isoMatrix = Matrix4x4.Rotate(Quaternion.Euler(0, 45, 0));
+    public static Vector3 ToIso(this Vector3 input) => _isoMatrix.MultiplyPoint3x4(input);
+}
+
+
+
