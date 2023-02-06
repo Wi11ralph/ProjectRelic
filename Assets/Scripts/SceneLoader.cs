62cef9dce9d560f85bfb3bf5bec22675e490947f
William Ralph (40165278) <40165278@hcuc.ac.uk>
2023-02-06 14:35:31 +0000

    nyaaaa

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -6,11 +6,14 @@ public class SceneLoader : MonoBehaviour
 {
     //private string scenePath = "Assets/Scenes/";
     [SerializeField] private string scene = "Room2";
-
+    [SerializeField] GameObject player;
+    public static Vector3 pos;
+    public static Vector3 rot;
     void OnGUI()
     {
         if (GUI.Button(new Rect(10, 10, 100, 30), "Change Scene"))
         {
+            pos = player.transform.position;
             Debug.Log("Scene loading: " + scene);
             SceneManager.LoadScene(scene, LoadSceneMode.Single);
         }
