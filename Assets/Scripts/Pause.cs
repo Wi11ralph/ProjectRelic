d468b26ee638a71b59f7432743cef9b86b9a2506
Wi11ralph <40165278@hcuc.ac.uk>
2023-03-01 17:50:00 +0000

    button animation
    button animation when you hover over the buttons, the lines will slide side to side, a bit experimental but I thought it looked neat

--- Assets/Scripts/Pause.cs
+++ Assets/Scripts/Pause.cs
@@ -10,6 +10,9 @@ public class Pause : MonoBehaviour
     public static bool active;
     private bool wait;
     private float scaleTime = 1f;
+
+    [SerializeField] private CanvasGroup tint;
+    [SerializeField] private GameObject t;
     private enum Button
     {
         Continue,
@@ -17,17 +20,85 @@ public class Pause : MonoBehaviour
         Menu,
         None
     }
+    public class Bttn
+    {
+        private GameObject L1;
+        private GameObject L2;
+        private Vector3 L1StartPos;
+        private Vector3 L2StartPos;
+        private float top;
+        private float bottom;
+
+        
+        public Bttn (GameObject buton,float yMax,float yMin)
+        {
+            top    = yMax;
+            bottom = yMin;
+         
+            for(int i=0; i < buton.transform.childCount; i++)
+            {
+                if(buton.transform.GetChild(i).name == "L1")
+                {
+                    L1 = buton.transform.GetChild(i).gameObject;
+                    L1StartPos = L1.transform.localPosition;
+                }
+                if (buton.transform.GetChild(i).name == "L2")
+                {
+                    L2 = buton.transform.GetChild(i).gameObject;
+                    L2StartPos = L2.transform.localPosition;
+                }
+            }
+        }
+        private void Offset(float amount)
+        {
+            L1.transform.localPosition = Vector3.Lerp(L1.transform.localPosition, L1StartPos - new Vector3(-amount, 0f, 0f), 5f * Time.unscaledDeltaTime);
+            L2.transform.localPosition = Vector3.Lerp(L2.transform.localPosition, L2StartPos - new Vector3( amount, 0f, 0f), 5f * Time.unscaledDeltaTime);
+        }
+        private void Reset()
+        {
+            L1.transform.localPosition = Vector3.Lerp(L1.transform.localPosition, L1StartPos, 8f * Time.unscaledDeltaTime);
+            L2.transform.localPosition = Vector3.Lerp(L2.transform.localPosition, L2StartPos, 8f * Time.unscaledDeltaTime);
+        }
+        public bool Hover(bool horizontalCheck)
+        {
+            if (bottom <= Input.mousePosition.y && Input.mousePosition.y <= top && horizontalCheck)
+            { 
+                Offset(50f);
+                return true;
+            }
+            else
+            {
+                Reset();
+                return false;
+            }
+        }
+    }
     private Button bttn;
+    private Button lastBttn;
 
-    [SerializeField] private CanvasGroup tint;
-    [SerializeField] private GameObject t;
+    
 
-    [SerializeField] private GameObject button1;
-    [SerializeField] private GameObject button2;
-    [SerializeField] private GameObject button3;
+    [SerializeField] private GameObject bt1;
+    [SerializeField] private GameObject bt2;
+    [SerializeField] private GameObject bt3;
 
+    private Bttn button1;
+    private Bttn button2;
+    private Bttn button3;
+    private bool xLined()
+    {
+        if (235 + t.transform.localPosition.x <= Input.mousePosition.x && Input.mousePosition.x <= 760 + t.transform.localPosition.x) return true;
+        else return false;
+    }
+    private void Start()
+    {
+        button1 = new Bttn(bt1,660,560);
+        button2 = new Bttn(bt2,485,385);
+        button3 = new Bttn(bt3,310,210);
+    }
     void Update()
     {
+        
         IfHover();
         GatherInput();
 
@@ -45,35 +116,17 @@ public class Pause : MonoBehaviour
             if (scaleTime >= 0.99999) scaleTime = 1;
             t.transform.localPosition = new(Mathf.Lerp(t.transform.localPosition.x, -600f, 5f * Time.unscaledDeltaTime), 0f, 0f);
         }
-        Debug.Log(t.transform.localPosition);
-
-        //Debug.Log(scaleTime);
-        //Debug.Log(Time.timeScale);
         Time.timeScale = scaleTime;
     }
     private void IfHover()
     {
-        if (235 + t.transform.localPosition.x <= Input.mousePosition.x && Input.mousePosition.x <= 760 + t.transform.localPosition.x)
+        lastBttn = bttn;
 
-        {
-            if (560 <= Input.mousePosition.y && Input.mousePosition.y <= 660)
-            {
-                //button 1
-                bttn = Button.Continue;
-            }
-            else if (385 <= Input.mousePosition.y && Input.mousePosition.y <= 485)
-            {
-                //button 2
-                bttn = Button.Restart;
-            }
-            else if (210 <= Input.mousePosition.y && Input.mousePosition.y <= 310)
-            {
-                //button 3
-                bttn = Button.Menu;
+        bttn = Button.None;
+        if(button1.Hover(xLined())) bttn = Button.Continue;
+        if(button2.Hover(xLined())) bttn = Button.Restart;
+        if(button3.Hover(xLined())) bttn = Button.Menu;
 
-            } else bttn = Button.None;
-        } else bttn = Button.None;
-              
     }
     private void Pauser()
     {
